ef686f1f5930cbcc8d1b7b7883766b4d3ed63e12
Ernesto <90488775+ErnestoAbreu@users.noreply.github.com>
2023-04-06 16:06:38 -0400

    Added Matrix.cs and Vector.cs

---
+++ MoogleEngine/Matrix.cs
@@ -0,0 +1,80 @@
+namespace MoogleEngine;
+
+public class Matrix
+{
+    private int rows;
+    private int columns;
+    public double[,] matrix;
+
+    // Constructor de la matriz
+    public Matrix(int rows, int columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        this.matrix = new double[rows, columns];
+    }
+
+    // Definicion del operador indizador
+    public double this[int i, int j]
+    {
+        get { return matrix[i, j]; }
+        set { matrix[i, j] = value; }
+    }
+
+    // Definicion de multiplicacion de matrices
+    public static Matrix operator *(Matrix A, Matrix B)
+    {
+        if (A.columns != B.rows)
+        {
+            // Exception
+            return new Matrix(0, 0);
+        }
+        else
+        {
+            Matrix C = new Matrix(A.rows, B.columns);
+            for (int i = 0; i < C.rows; ++i)
+                for (int j = 0; j < C.columns; ++j)
+                    for (int k = 0; k < A.columns; ++k)
+                    {
+                        C[i, j] += A[i, k] * B[k, j];
+                    }
+            return C;
+        }
+    }
+
+    // Definicion de multiplicacion de un escalar por una matriz
+    public static Matrix operator *(double x, Matrix A)
+    {
+        Matrix C = new Matrix(A.rows, A.columns);
+
+        for (int i = 0; i < A.rows; ++i)
+            for (int j = 0; j < A.columns; ++j)
+                C[i, j] = A[i, j] * x;
+
+        return C;
+    }
+
+    // Definicion de matriz transpuesta
+    public void Transpose()
+    {
+        Matrix newMatrix = new Matrix(this.columns, this.rows);
+
+        for (int i = 0; i < this.rows; ++i)
+            for (int j = 0; j < this.columns; ++j)
+                newMatrix[j, i] = this[i, j];
+    }
+
+    // Imprime la matriz
+    public void debug()
+    {
+        for (int i = 0; i < this.rows; ++i)
+        {
+            for (int j = 0; j < this.columns; ++j)
+            {
+                Console.Write(this[i, j]);
+                Console.Write(' ');
+            }
+            Console.Write('\n');
+        }
+    }
+}
---
+++ MoogleEngine/Vector.cs
@@ -0,0 +1,34 @@
+namespace MoogleEngine;
+
+public class Vector : Matrix
+{
+    public int Dimensions;
+
+    public Vector(int N) : base(1, N)
+    {
+        this.Dimensions = N;
+    }
+    public double this[int i]
+    {
+        get { return matrix[0,i]; }
+        set { matrix[0,i] = value; }
+    }
+
+    static public double Dot_Product(Vector a, Vector b) { 
+        if(a.Dimensions != b.Dimensions) return 0;
+        
+        double dot_product = 0;
+        for(int i = 0; i < a.Dimensions; ++i)
+            dot_product += a[i] * b[i];
+        
+        return dot_product;
+    }
+
+    public double Module() {
+        double module = 0;
+        for(int i = 0; i < this.Dimensions; i++) {
+            module += this[i] * this[i];
+        }
+        return Math.Sqrt(module);
+    }
+}
